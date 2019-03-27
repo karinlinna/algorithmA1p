a26809459c6b2ea2ddb5210733b45097f8bb6d63
Karin0816 <karinlinna@gmail.com>
2019-03-27 13:48:17 +0800

    3/27

---
+++ week1/Analyse of algorithm/1.java
@@ -0,0 +1,7 @@
+/*
+-FFT 
+-3-SUM问题
+    简单代码的写法是有三个for循环
+    如何看这个程序的时间，java在它的类中有一个秒表类 stopwatch
+    Stopwatch stopwatch = new Stopwatch ();
+    double time = stopwatch.elasedTime();*/
---
+++ week2/Stack.java
@@ -0,0 +1,8 @@
+public class Stack
+{
+    stack()
+    void push(String item)
+    String pop()
+    Boolean IsEmpty()
+    int size
+}
